ae1fdf308e5f21e1a716c1ba5917ea4eac32f3a3
Artem Kirillov <74914141+airyou-code@users.noreply.github.com>
2022-09-18 18:20:34 +0200

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/', views.IndexView.as_view()),
+    path('', views.IndexView.as_view()),
     path('cancel/', views.CancelView.as_view(), name='cancel'),
     path('success/', views.SuccessView.as_view(), name='success'),
     path('item/<int:pk>/', views.item),
