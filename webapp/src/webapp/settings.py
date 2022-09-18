6dcfffc5bc4cac1725b4b8d88ffaf4d2bed7eb8e
airyou-code <holod7272@outlook.com>
2022-09-18 05:07:41 +0200

    heroku

--- webapp/src/webapp/settings.py
+++ webapp/src/webapp/settings.py
@@ -29,7 +29,7 @@ STRIPE_SECRET_KEY = STRIPE_SECRET_KEY
 STRIPE_PUBLISH_KEY = STRIPE_PUBLISH_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
